bf23850c1c40821f55ffc9f9280e65458a037173
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-01 15:47:16 +1000

    updated routes file

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,28 @@
 const router = require("express").Router();
-const workoutdb = require("../models/workout.js");
+const mongoose = require("mongoose");
+const Workout = require("../models/workout.js");
+
+// Create new workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+// Update workout
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+	Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
 // get workouts
 router.get("/api/workouts", (req, res) => {
@@ -12,15 +35,14 @@ router.get("/api/workouts", (req, res) => {
       });
 });
 
-
-
-// Create new workout
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+router.get("/api/workouts/range", (req, res) => {
+	Workout.find()
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
+
+module.exports = router;
