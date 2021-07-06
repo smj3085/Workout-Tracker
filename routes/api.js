e444407082854a8a87a734f5eafdb2812172b289
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-06 20:51:28 +1000

    updated api routes

--- routes/api.js
+++ routes/api.js
@@ -25,23 +25,42 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
 
 // get workouts
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
+	Workout.aggregate([{
+			$addFields: {
+				totalDuration: {
+					$sum: "$exercises.duration"
+				},
+			},
+		}])
+		.then(dbWorkout => {
+			res.json(dbWorkout);
+		})
+		.catch(err => {
+			res.status(400).json(err);
+		});
 });
 
 router.get("/api/workouts/range", (req, res) => {
-	Workout.find()
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+	Workout.aggregate([{
+      $addFields: {
+        totalDuration: { 
+          $sum: "$exercises.duration" 
+        },
+      },
+    },
+    {
+      $sort: {
+        day: -1,
+      },
+    },
+    { $limit: 7 },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
